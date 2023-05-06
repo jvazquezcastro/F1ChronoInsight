1dc0536b0a0a00dba9026901678f4dd1e4e97116
jvazquezcastro <jesusvazquezcastro22@gmail.com>
2023-05-06 21:39:56 +0200

    Creacion proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { FrontPageComponent } from './shared/components/front-page/front-page.component';
+import { MaterialModule } from './material.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FrontPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports:[]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
+import { MatDividerModule} from '@angular/material/divider'
+import { MatGridListModule} from '@angular/material/grid-list'
+
+
+
+@NgModule({
+  exports:[
+    MatToolbarModule,
+    MatIconModule,
+    MatDividerModule,
+    MatGridListModule
+  ]
+})
+export class MaterialModule { }
