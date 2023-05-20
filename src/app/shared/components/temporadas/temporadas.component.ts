ddc52f0de27c5bafceb1f112f14850b13c619d54
jvazquezcastro <jesusvazquezcastro22@gmail.com>
2023-05-20 15:58:41 +0200

    .

--- src/app/shared/components/temporadas/temporadas.component.ts
+++ src/app/shared/components/temporadas/temporadas.component.ts
@@ -12,14 +12,13 @@ export class TemporadasComponent {
 
   selectedYear: string = '';
   years = ["2023", "2022", "2021", "2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010", "2009", "2008", "2007", "2006", "2005", "2004", "2003", "2002", "2001", "2000"];
-
   constructor(private pilotoService: PilotoService) {}
 
   onYearSelected(year: string) {
-    console.log('Year selected:', year);
-    this.selectedYear = year;
-    console.log(this.pilotos)
-    this.loadDrivers();
+      console.log('Year selected:', year);
+      this.selectedYear = year;
+      console.log(this.pilotos)
+      this.loadDrivers();
   }
 
   loadDrivers() {
